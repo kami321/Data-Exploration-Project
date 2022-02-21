befb086acbc84f03dfcbc3c17ac72d1437323085
KamiYang <81647911+kami321@users.noreply.github.com>
2022-02-20 22:35:53 -0800

    Add files via upload

---
+++ Analysis Code/Regression Anaylsis.R
@@ -0,0 +1,43 @@
+# Load relevant libraries
+library(tidyverse)
+library(vtable)
+library(jtools)
+library(car)
+
+My_Cleaned_Data <- read_csv("../kami/My_Cleaned_Data.csv")
+
+# lookup data at vtable
+vtable(My_Cleaned_Data) 
+
+My_Cleaned_Data <- My_Cleaned_Data %>%
+  mutate(organization = factor(organization))
+
+# Regression model 1
+Model1 <- lm(formula = index_tot ~ College_Scorecard + high_earning + time_v + College_Scorecard*time_v + College_Scorecard*high_earning, data = My_Cleaned_Data)
+export_summs(Model1, digits = 3, robust = TRUE)
+
+# Hypothesis test: if the effect of College_Scorecard on index scores is 0
+linearHypothesis(Model1, "College_Scorecard = 0", white.adjust = TRUE)
+
+# Organization, filter: removed 63,500 rows (97%), 1,785 rows remaining
+organization <- My_Cleaned_Data %>% 
+  filter(!duplicated(organization)) %>%
+  summarize()
+
+# Summary Statistics for comprehensive index and time
+summary(My_Cleaned_Data[c("index_tot", "time_v")]) 
+
+# Standard Deviation [1] 0.5791552
+sd(My_Cleaned_Data$index_tot)
+
+# Correlation between College_Scorecard, high_earning, time_v
+cor(My_Cleaned_Data[c("College_Scorecard", "high_earning", "time_v")])
+hist(My_Cleaned_Data$index_tot)
+
+# Regression results: differences between high-earning and low-earning college graduates
+ggplot(data = My_Cleaned_Data, aes(x = College_Scorecard, y = index_tot,
+                                   group= high_earning, color=factor(high_earning))) + 
+  geom_line() + 
+  xlab("College Scorecard") + 
+  ylab("Comprehensive Index") + 
+  scale_color_discrete(name = "high_earning", labels = c("Low Earning", "High Earning"))
